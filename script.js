af1c4e02d91b45472e24f4f94370d0a44b384324
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-07-26 20:01:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  let ans = "";
+
+  if (num === 0) {
+    return "0";
+  }
+
+  while (num) {
+    let val = Math.floor(num % 2);
+    num = Math.floor(num / 2);
+    ans += val;
+  }
+
+  return ans.split('').reverse().join('');
 }
 
 window.decimalToBinary = decimalToBinary;
